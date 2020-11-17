d68fc10d831ac2a3088d6354036dd14fc42ce117
61070339 Copter <61070339@kmitl.ac.th>
2020-11-17 12:13:54 +0700

    Update test.py

--- Arc/test.py
+++ Arc/test.py
@@ -1,7 +1,7 @@
 print("Origin Distance width(px): ", end="")
 print(str((float(input())*100)/1920)+"vw")
-print("Origin Distance height(px): ", end="")
-print(str((float(input())*100)/1080)+"vh")
+for _ in range(10000):
+    print("Change vh to vw (vh): ", end="")
+    print(str(((float(input()[:-2])*1080)/100*100)/1920)+"vw")
 print("Font Size(pt): ", end="")
 print(str(((float(input())*1.3281472327365)*100)/1920)+"vw")
-

49b49354480e44a7faf6a629ed32c20cb7e0ee84
61070339 Copter <61070339@kmitl.ac.th>
2020-11-17 10:52:52 +0700

    ...

--- Arc/test.py
+++ Arc/test.py
@@ -2,3 +2,6 @@ print("Origin Distance width(px): ", end="")
 print(str((float(input())*100)/1920)+"vw")
 print("Origin Distance height(px): ", end="")
 print(str((float(input())*100)/1080)+"vh")
+print("Font Size(pt): ", end="")
+print(str(((float(input())*1.3281472327365)*100)/1920)+"vw")
+
